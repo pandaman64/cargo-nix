4978d09bcf7913266b037215c51f361aaccb9e01
pandaman64 <kointosudesuyo@infoseek.jp>
2021-01-25 19:15:01 +0900

    refactor

---
+++ src/crates_io.rs
@@ -0,0 +1,53 @@
+use super::Result;
+
+use std::path::{Path, PathBuf};
+
+use flate2::read::GzDecoder;
+use serde::Deserialize;
+use tar::Archive;
+
+#[derive(Debug, Deserialize)]
+pub struct Version {
+    pub dl_path: String,
+    #[serde(rename = "crate")]
+    pub krate: String,
+    pub num: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Versions {
+    pub versions: Vec<Version>,
+}
+
+const CRATES_IO_ROOT: &str = "https://crates.io";
+
+fn versions(crate_name: &str) -> String {
+    format!("{}/api/v1/crates/{}/versions", CRATES_IO_ROOT, crate_name)
+}
+
+fn downloads(dl_path: &str) -> String {
+    format!("{}{}", CRATES_IO_ROOT, dl_path)
+}
+
+pub fn retrieve_crate_versions(crate_name: &str) -> Result<Versions> {
+    ureq::get(&versions(crate_name))
+        .set("User-Agent", "cargo-nix")
+        .call()?
+        .into_json()
+        .map_err(Into::into)
+}
+
+pub fn unpack_crate(base: &Path, version: &Version) -> Result<()> {
+    let krate = ureq::get(&downloads(&version.dl_path))
+        .set("User-Agent", "cargo-nix")
+        .call()?
+        .into_reader();
+    let mut krate = Archive::new(GzDecoder::new(krate));
+    krate.unpack(base).map_err(Into::into)
+}
+
+pub fn crate_path(base: &Path, version: &Version) -> PathBuf {
+    let mut buf = base.to_path_buf();
+    buf.push(format!("{}-{}", version.krate, version.num));
+    buf
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-use flate2::read::GzDecoder;
-use serde::Deserialize;
-use tar::Archive;
-
 use std::{path::Path, process::Command};
 
-#[derive(Debug, Deserialize)]
-struct Version {
-    dl_path: String,
-    num: String,
-}
+pub type Result<T, E = anyhow::Error> = anyhow::Result<T, E>;
 
-#[derive(Debug, Deserialize)]
-struct Versions {
-    versions: Vec<Version>,
-}
-
-const API_ROOT: &str = "https://crates.io";
-
-fn versions(crate_name: &str) -> String {
-    format!("{}/api/v1/crates/{}/versions", API_ROOT, crate_name)
-}
-
-fn downloads(dl_path: &str) -> String {
-    format!("{}{}", API_ROOT, dl_path)
-}
+mod crates_io;
+mod nix;
 
 fn run_crate2nix<P: AsRef<Path>>(path: P) {
     Command::new("crate2nix")
@@ -44,26 +24,14 @@ fn nix_build<P: AsRef<Path>>(path: P) {
 fn main() -> anyhow::Result<()> {
     let crate_name = "ripgrep";
 
-    let versions: Versions = ureq::get(&versions(crate_name))
-        .set("User-Agent", "cargo-nix")
-        .call()?
-        .into_json()?;
+    let versions = crates_io::retrieve_crate_versions(crate_name)?;
     let latest = &versions.versions[0];
 
     let tempdir = tempfile::tempdir()?;
-    let crate_path = {
-        let mut path = tempdir.path().to_owned();
-        path.push(format!("{}-{}", crate_name, latest.num));
-        path
-    };
+    let crate_path = crates_io::crate_path(tempdir.path(), latest);
     println!("{}", crate_path.display());
 
-    let krate = ureq::get(&downloads(&latest.dl_path))
-        .set("User-Agent", "cargo-nix")
-        .call()?
-        .into_reader();
-    let mut krate = Archive::new(GzDecoder::new(krate));
-    krate.unpack(tempdir.path())?;
+    crates_io::unpack_crate(tempdir.path(), latest)?;
 
     run_crate2nix(&crate_path);
     nix_build(&crate_path);
