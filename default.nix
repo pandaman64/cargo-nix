d53ff728647c6883f61a4be0f2525b8dc7de3c25
pandaman64 <kointosudesuyo@infoseek.jp>
2021-01-25 23:55:46 +0900

    Nix package

---
+++ default.nix
@@ -0,0 +1,17 @@
+# A derivation for the wrapper shell script
+# taken from: https://nixos.wiki/wiki/Nix_Cookbook
+{ pkgs ? import ./nix/nixpkgs.nix { } }:
+let
+  sources = import ./nix/sources.nix;
+  crate2nix = pkgs.callPackage sources.crate2nix { };
+  cargoNix = pkgs.callPackage (import ./Cargo.nix) { };
+in cargoNix.rootCrate.build.overrideAttrs
+(oldAttrs: {
+  buildInputs = (oldAttrs.buildInputs or []) ++ [
+    pkgs.makeWrapper
+  ];
+  postInstall = (oldAttrs.postInstall or "") + ''
+    wrapProgram $out/bin/cargo-nix \
+      --prefix PATH : ${crate2nix}/bin
+  '';
+})
