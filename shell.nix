ea10d74dd60628ea61ea551133d122a7c297e5b8
pandaman64 <kointosudesuyo@infoseek.jp>
2021-04-25 01:11:32 +0900

    add niv to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ let
   sources = import nix/sources.nix;
   crate2nix = pkgs.callPackage sources.crate2nix { };
 in pkgs.mkShell {
-  buildInputs = [ pkgs.nixfmt crate2nix ]
+  buildInputs = [ pkgs.nixfmt pkgs.niv crate2nix ]
     ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
       pkgs.darwin.apple_sdk.frameworks.Security
       pkgs.darwin.apple_sdk.frameworks.CoreServices
